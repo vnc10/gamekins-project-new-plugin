0241bc6134fb2085ba2d1b6a4f34795f0bdcb401
jgtsuchiya <joaog.tsuchiya@gmail.com>
2025-12-02 18:41:59 -0300

    test: add shouldFindAllSubject test to CouseServiceTest

--- src/test/java/gamekins/project/service/CouseServiceTest.java
+++ src/test/java/gamekins/project/service/CouseServiceTest.java
@@ -1,12 +1,21 @@
 package gamekins.project.service;
 
+import gamekins.project.domain.Course;
+import gamekins.project.domain.dto.CourseDTO;
+import gamekins.project.domain.dto.SubjectDTO;
 import gamekins.project.repository.CourseRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -32,4 +41,26 @@ class CouseServiceTest {
         verify(courseRepository).deleteById(id);
     }
 
+    @Test
+    void shouldFindAllSubject() {
+        Course course1 = new Course();
+        course1.setId(1L);
+        course1.setName("Teste1");
+        course1.setCode("001");
+
+        Course course2 = new Course();
+        course2.setId(2L);
+        course2.setName("Teste2");
+        course2.setCode("002");
+
+        List<Course> courses = Arrays.asList(course1, course2);
+
+        when(courseRepository.findAll()).thenReturn(courses);
+
+        List<CourseDTO> result = courseService.findAll();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("Teste1", result.get(0).getName());
+    }
 }
