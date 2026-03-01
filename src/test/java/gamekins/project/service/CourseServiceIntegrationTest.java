b5a72463b9a54b5b279703f925e16f59c4704b53
lucassilvanav9 <lucas.silva@nav9.tech>
2026-03-01 20:31:53 -0300

    change test to use parameterized test

--- src/test/java/gamekins/project/service/CourseServiceIntegrationTest.java
+++ src/test/java/gamekins/project/service/CourseServiceIntegrationTest.java
@@ -4,9 +4,13 @@ import gamekins.project.domain.Course;
 import gamekins.project.integration.MySQLTestContainer;
 import gamekins.project.repository.CourseRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -21,11 +25,12 @@ public class CourseServiceIntegrationTest extends MySQLTestContainer {
     @Autowired
     private CourseRepository courseRepository;
 
-    @Test
-    void shouldDeleteCourseSuccessfully() {
+    @ParameterizedTest
+    @ValueSource(strings = {"code-001", "code-002", "code-003", "code-004"})
+    void shouldDeleteCourseSuccessfully(String code) {
         Course course = new Course();
         course.setName("Curso de Teste");
-        course.setCode("312");
+        course.setCode(code);
         course = courseRepository.save(course);
         Long id = course.getId();
 
